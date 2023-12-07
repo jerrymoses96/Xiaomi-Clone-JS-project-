5c9fd1544cffa357fb4c8d39bb7abec0fa68e991
jerin <jerinkunnamkada@gmail.com>
2023-12-07 10:56:32 +0530

    login page can now handle mutliple data

--- login.js
+++ login.js
@@ -1,20 +1,40 @@
+// function checkData() {
+//     var enterUsername = document.getElementById("username").value;
+//     var enterPass = document.getElementById("pwd").value;
+
+//     // GETTING DATA FROM LOCAL STORAGE 
+//     var getUsername = localStorage.getItem('userName');
+//     var getPass = localStorage.getItem('userPwd');
+//     if (enterUsername == getUsername) {
+//         if(enterPass == getPass ){
+//             alert("Login Successful");
+//         }
+//         else{
+//             alert("Invalid Password");
+//         }
+        
+//     }
+//     else{
+//         alert("User not found");
+//     }
+// }
+
+
 function checkData() {
     var enterUsername = document.getElementById("username").value;
     var enterPass = document.getElementById("pwd").value;
-
+  
     // GETTING DATA FROM LOCAL STORAGE 
-    var getUsername = localStorage.getItem('userName');
-    var getPass = localStorage.getItem('userPwd');
-    if (enterUsername == getUsername) {
-        if(enterPass == getPass ){
-            alert("Login Successful");
-        }
-        else{
-            alert("Invalid Password");
-        }
-        
+    var userData = JSON.parse(localStorage.getItem('userData')) || [];
+  
+    var foundUser = userData.find(function(user) {
+      return user.userName === enterUsername && user.userPwd === enterPass;
+    });
+  
+    if (foundUser) {
+      alert("Login Successful");
+    } else {
+      alert("Invalid Username or Password");
     }
-    else{
-        alert("User not found");
-    }
-}
+  }
+  
--- registration.js
+++ registration.js
@@ -1,9 +1,28 @@
+
+// function addData() {
+//   var username = document.getElementById("username").value;
+//   var pass = document.getElementById("pwd").value;
+
+//   // Adding data to local storage
+//   localStorage.setItem("userName", username);
+//   localStorage.setItem("userPwd", pass);
+// }
+
 function addData() {
-  var Username = document.getElementById("username").value;
+  var username = document.getElementById("username").value;
   var pass = document.getElementById("pwd").value;
 
-  //   adding data to local storage
+  // Retrieve existing data from local storage
+  var existingData = JSON.parse(localStorage.getItem("userData")) || [];
 
-  localStorage.userName = Username;
-  localStorage.userPwd = pass;
+  // Add new data to the array
+  var newData = {
+    userName: username,
+    userPwd: pass
+  };
+
+  existingData.push(newData);
+
+  // Save the updated array back to local storage
+  localStorage.setItem("userData", JSON.stringify(existingData));
 }
