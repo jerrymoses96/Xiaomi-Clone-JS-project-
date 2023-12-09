48f91f1a43f1ab812006b1cdeba30eb3897b66bc
jerin <jerinkunnamkada@gmail.com>
2023-12-09 19:51:07 +0530

    some changes

--- login.js
+++ login.js
@@ -1,23 +1,3 @@
-// function checkData() {
-//     var enterUsername = document.getElementById("username").value;
-//     var enterPass = document.getElementById("pwd").value;
-
-//     // GETTING DATA FROM LOCAL STORAGE 
-//     var getUsername = localStorage.getItem('userName');
-//     var getPass = localStorage.getItem('userPwd');
-//     if (enterUsername == getUsername) {
-//         if(enterPass == getPass ){
-//             alert("Login Successful");
-//         }
-//         else{
-//             alert("Invalid Password");
-//         }
-        
-//     }
-//     else{
-//         alert("User not found");
-//     }
-// }
 
 
 function checkData() {
